8898a8e990c182490c8412932d1b69212a8de836
Shahid Nazir <shahidnazirmhd@gmail.com>
2024-12-14 16:49:40 +0400

    basics with Conditions and loops

--- blockchain.py
+++ blockchain.py
@@ -4,25 +4,54 @@ blockchain = []
 
 def get_last():
     """Get last stored transaction"""
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
 def add_value(value, last_value=[1]):
     """To store new transaction"""
+    if last_value == None:
+        last_value = [1]
     blockchain.append([last_value, value])
 
 
-def get_user_input():
+def get_transaction_value():
     """Get user input for new transaction to be stored"""
     return float(input("Enter Transaction Amount: "))
 
 
-tran_amt = get_user_input()
-add_value(tran_amt)
-tran_amt = get_user_input()
-add_value(tran_amt, get_last())
-tran_amt = get_user_input()
-add_value(last_value=get_last(),
-           value=tran_amt)
+def get_user_input():
+    """Get user choice"""
+    return input("What you choose: ")
 
-print(blockchain)
+
+def print_blockchain_elements():
+    #Output
+    print("Outputing Block")
+    for block in blockchain:
+        print(block)
+    print("-" * 25)    
+
+
+while True:
+    print("Please choose")
+    print("1: Add a new transaction")
+    print("2: Output the blockchain blocks")
+    print("q: Quit")
+    user_choice = get_user_input()
+    if user_choice == "1":
+        tran_amt = get_transaction_value()
+        add_value(tran_amt, get_last())
+    elif user_choice == "2":
+        print_blockchain_elements()
+    elif user_choice == "q":
+        break
+    else:
+        print("Input was invalid, Pease pick correct")
+        continue
+    print("Choice fulfilled!")    
+
+
+print("Done!")
+
